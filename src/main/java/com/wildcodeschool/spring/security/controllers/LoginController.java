33f434322915fec21bd2b3d4073fe5474d97fb00
lgrignon <louis.grignon@protonmail.com>
2023-03-02 15:55:48 +0100

    clean code

--- src/main/java/com/wildcodeschool/spring/security/controllers/LoginController.java
+++ src/main/java/com/wildcodeschool/spring/security/controllers/LoginController.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
